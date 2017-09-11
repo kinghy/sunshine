647b4c3fb83e24ac03bc875a7f10a9276355c0ba
kinghy <17645998@qq.com>
2017-09-11 14:50:32 +0800

    s10

--- js/s10.js
+++ js/s10.js
@@ -23,7 +23,7 @@ $().ready(function () {
         })
     });
 
-    var fs = ChoiceStage.init("first_qa",138,144,$("#zl_btn"),$("#jz_long_btn,#yj_btn"),145.5,149.5,150,154,154.5,160);
+    var fs = ChoiceStage.init("first_qa",138,144,$("#zl_btn"),$("#jz_long_btn,#yj_btn"),145.5,149.5,150.3,154,154.5,160);
     var ss = Stage.init("start_2",161,169,function (s) {//初始化
         $("#start_qd_2").click(function (e) {
             this.src = "resource/qd_highlight.png";
@@ -53,39 +53,111 @@ $().ready(function () {
             }.bind(this),500)
         })
     });
-    var sevens = Stage.init("fourth_qa",241,247,function (s) {//初始化
-        // var c=document.getElementById("f_canvas");
-        // var ctx=c.getContext("2d");
-        // var cxs = new Image();
-        // cxs.src = "resource/cxs.png";
-        // cxs.onload=function(){
-        //     ctx.drawImage(cxs,10,10,198,70);
-        // };
-        // cxs.onclick = function () {
-        //     alert(123);
-        // }
-        // var gls = new Image();
-        // gls.src = "resource/gls.png";
-        // gls.onload=function(){
-        //     ctx.drawImage(gls,10,120,198,70);
-        // };
-        // var whs = new Image();
-        // whs.src = "resource/whs.png";
-        // whs.onload=function(){
-        //     ctx.drawImage(whs,10,230,198,70);
-        // };
-        var canvas = new fabric.Canvas('f_canvas');
-        fabric.Image.fromURL('resource/cxs.png', function(img) {
-            img.set({
-                left: 10,
-                top: 10
-            });
-            canvas.add(img);
-        });
 
+    var lineAnswer = new Array();
+    var $g1 = null;
+    var $g2 = null;
+    var $g1Sel = null;
+    var $g2Sel = null;
+    var sevens = CustomChoiceStage.init("fourth_qa",241,247,$("#q4_submit"),function () {
+        return (lineAnswer["cxs"]=="bjdl" && lineAnswer["gls"]=="czjz" && lineAnswer["whs"] == "ygzh")
+    },249,259.5,260.5,264,264.5,270,function (s) {//初始化
+        var $s = $("#"+s.stageId);
+        $g1 = $s.find(".g1");
+        $g2 = $s.find(".g2");
+        var selectGroup = function ($this,$g) {
+            if($this.hasClass("selected")){
+                $this.removeClass("selected");
+                $this.attr("src","resource/"+$this.attr("data")+".png");
+                return null;
+            }else{
+                $g.each(function () {
+                    var $t = $(this)
+                    if($t.hasClass("selected")){
+                        $t.removeClass("selected");
+                        $t.attr("src","resource/"+$t.attr("data")+".png");
+                    }
+                })
+                $this.addClass("selected");
+                $this.attr("src","resource/"+$this.attr("data")+"_highlight.png");
+                return $this;
+            }
+        }
+
+        var drawLine = function () {
+            if($g1Sel!=null && $g2Sel!=null){
+                var canvas = document.getElementById("f_canvas");//获取到canvas元素
+                var context = canvas.getContext("2d");//获取上下文的环境
+                context.beginPath();
+                context.moveTo(0,0);
+                // context.lineTo(300,300);
+                var moveX = $g1Sel.position().left+$g1Sel.width()+10;
+                var moveY = $g1Sel.position().top+$g1Sel.height()/2;
+                var LineX = $g2Sel.position().left-10;
+                var LineY = $g2Sel.position().top+$g2Sel.height()/2;
+                context.moveTo(moveX,moveY);
+                context.lineTo(LineX,LineY);
+                context.strokeStyle = "#36D9FF"
+                context.lineWidth = 5;//线条的宽度
+                context.stroke();//绘制
+                context.closePath();
+                $g1Sel.removeClass("selected");
+                $g1Sel.addClass("lined");
+                $g1Sel.attr("src","resource/"+$g1Sel.attr("data")+".png");
+
+                $g2Sel.removeClass("selected");
+                $g2Sel.addClass("lined");
+                $g2Sel.attr("src","resource/"+$g2Sel.attr("data")+".png");
+
+                lineAnswer[$g1Sel.attr("data")]= $g2Sel.attr("data");
+                $g1Sel = null;
+                $g2Sel = null;
+            }
+        }
+
+        $g1.click(function () {
+            var $t = $(this)
+            if(!$t.hasClass("lined")){
+                $g1Sel = selectGroup($t,$g1);
+                setTimeout(function () {
+                    drawLine();
+                },300)
+            }
+        })
+        $g2.click(function () {
+            var $t = $(this)
+            if(!$t.hasClass("lined")){
+                $g2Sel = selectGroup($t,$g2);
+                setTimeout(function () {
+                    drawLine();
+                },300)
+            }
+        })
+
+    },function (s) {
+        var canvas = document.getElementById("f_canvas");//获取到canvas元素
+        var context = canvas.getContext("2d");//获取上下文的环境
+        context.clearRect(0,0,500,400);
+        lineAnswer = new Array();
+        $g1Sel = null;
+        $g2Sel = null;
+        $g1.removeClass("lined").removeClass("selected");
+        $g2.removeClass("lined").removeClass("selected");
     });
-    var es = Stage.init(null,340,0)
-    var sm = VideoStageManager.init("pageWrap","video",[zs,fs,ss,ts,fours,fives,sixs,sevens,es],function () {
+    var eights = Stage.init("start_5",271,284.5,function (s) {//初始化
+        $("#start_qd_5").click(function (e) {
+            this.src = "resource/qd_highlight.png";
+            setTimeout(function () {
+                this.src = "resource/qd.png"
+                s.playNext();
+            }.bind(this),500)
+        })
+    });
+    var nights = ChoiceStage.init("fifth_qa",285,291,$("#zysy_2_btn"),$("#gwdx_btn,#qyyq_btn"),292,307,308,311,311.5,317);
+
+
+    var es = Stage.init(null,317.5,0)
+    var sm = VideoStageManager.init("pageWrap","video",[zs,fs,ss,ts,fours,fives,sixs,sevens,eights,nights,es],function () {
         $("#end_page").show();
     });
 
