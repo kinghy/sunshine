6140c396c6054f4e20e78ddb028a7a6a65bff6c9
kinghy <17645998@qq.com>
2017-09-13 10:22:20 +0800

    testmedia

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ $().ready(function () {
         },false);
     }
     //事件监听
-    // eventTester("loadstart"); //客户端开始请求数据
+    eventTester("loadstart"); //客户端开始请求数据
     // eventTester("progress"); //客户端正在请求数据
     // eventTester("suspend"); //延迟下载
     // eventTester("abort"); //客户端主动终止下载（不是因为错误引起）
@@ -45,15 +45,15 @@ $().ready(function () {
     // eventTester("suspend"); //延迟下载
     eventTester("abort"); //客户端主动终止下载（不是因为错误引起），
     eventTester("error"); //请求数据时遇到错误
-    // eventTester("stalled"); //网速失速
+    eventTester("stalled"); //网速失速
     // eventTester("play"); //play()和autoplay开始播放时触发
     // eventTester("pause"); //pause()触发
-    // eventTester("loadedmetadata"); //成功获取资源长度
-    // eventTester("loadeddata"); //
+    eventTester("loadedmetadata"); //成功获取资源长度
+    eventTester("loadeddata"); //
     // eventTester("waiting"); //等待数据，并非错误
     // eventTester("playing"); //开始回放
-    // eventTester("canplay"); //可以播放，但中途可能因为加载而暂停
-    // eventTester("canplaythrough"); //可以播放，歌曲全部加载完毕
+    eventTester("canplay"); //可以播放，但中途可能因为加载而暂停
+    eventTester("canplaythrough"); //可以播放，歌曲全部加载完毕
     // eventTester("seeking"); //寻找中
     // eventTester("seeked"); //寻找完毕
     // eventTester("timeupdate"); //播放时间改变
